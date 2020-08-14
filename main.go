303153bbcbae0675e991dcad3330773f0b284cc6
mpapili <michaelpapili@gmail.com>
2020-08-14 14:45:12 -0700

    stand up a sample "GET" endpoint working with local ubuntu postgres

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -25,23 +26,31 @@ type Car struct {
 	Make string "json:string"
 }
 
+var db *sql.DB
+
 func init() {
-	fmt.Println("running init, nothing here now")
+	/*
+		connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%d sslmode=disable", user, password, dbname, host, port)
+		db, err := sql.Open("postgres", connStr)
+		checkErr(err)
+		fmt.Println(db)
+	*/
 }
 
 func checkErr(err error) {
 	// if error is not nill, log fatal error
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal(err)
 	}
 }
 
 func main() {
-	fmt.Println("hello!")
+	var err error
 
 	// let's test playing with postgres!
 	connStr := fmt.Sprintf("user=%s password=%s dbname=%s host=%s port=%d sslmode=disable", user, password, dbname, host, port)
-	db, err := sql.Open("postgres", connStr)
+	db, err = sql.Open("postgres", connStr)
 	checkErr(err)
 
 	// test hitting database test table
@@ -61,6 +70,7 @@ func main() {
 	// create mux router
 	router := mux.NewRouter()
 	router.HandleFunc("/test", testFunc).Methods("GET")
+	router.HandleFunc("/cars", getCars).Methods("GET")
 
 	// run the router
 	log.Fatal(http.ListenAndServe(":8000", router))
@@ -69,4 +79,24 @@ func main() {
 // handler for "/test"
 func testFunc(http.ResponseWriter, *http.Request) {
 	fmt.Println("you hit me!")
+}
+
+func getCars(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("running getCars")
+	var mycar Car // single book is of type book
+	cars := []Car{}
+
+	rows, err := db.Query("SELECT * FROM cars;")
+	checkErr(err)
+	fmt.Println("ran the query")
+	defer rows.Close() // close rows once this function is done
+
+	for rows.Next() {
+		fmt.Println("looking at another car")
+		err := rows.Scan(&mycar.ID, &mycar.Make)
+		checkErr(err)
+		cars = append(cars, mycar)
+	}
+	json.NewEncoder(w).Encode(cars)
 }
